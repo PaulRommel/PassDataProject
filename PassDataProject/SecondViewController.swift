71f2db423b7aff094b8ec6ecfc0b20d90e8ab72d
Paul <paulrommel87@gmail.com>
2021-03-04 20:13:10 +0300

    Project creation. Adding elements of views. Configuring the features. Creation of the necessary classes and outlets.

---
+++ PassDataProject/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Павел Попов on 04.03.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBAction func goBackTapped(_ sender: UIButton) {
+    }
+    
+}
